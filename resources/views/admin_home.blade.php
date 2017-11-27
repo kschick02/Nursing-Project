731301d2a62d774bb5e89eabf2a2224b7b77f380
peggysmith20 <peggysmith20@gmail.com>
2017-11-27 10:15:07 -0600

    Update admin_home.blade.php

--- resources/views/admin_home.blade.php
+++ resources/views/admin_home.blade.php
@@ -5,7 +5,7 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading"><h1>Admin Page</h1></div>
+                <div class="panel-heading"><h1>Administrative Page</h1></div>
 
                 <div class="panel-body">
                     @if (session('status'))
@@ -17,14 +17,14 @@
                     <h4>Patients:</h4>
                   <div>
                   <a href='/patients/index'>View all patients</a><br>
-                  <a href='/patients/create'>Create new patient</a><br>
+                  <a href='/patients/create'>Create New Patient</a><br>
                 </div>
                 <h4>Orders:</h4>
                   <a href='/orders/index'>View all orders</a><br>
-                  <a href='/orders/create'>Create new order</a><br>
+                  <a href='/orders/create'>Create New Order</a><br>
                   <h4>Labs:</h4>
                    <a href='/labs/index'>View all labs</a><br>
-                   <a href='labs/create'>Create new lab</a>
+                   <a href='labs/create'>Create New Lab</a>
 
                  </div>
                 </div>
